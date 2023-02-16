37a7c310d6374063ad2ce1c3135697c5552b959a
tails1022 <124885566+tails1022@users.noreply.github.com>
2023-02-15 23:56:30 -0500

    Curso :D

---
+++ Curso Visual Studio Code/Clases definiciones/04-replace-definition.ts
@@ -0,0 +1,25 @@
+import { SuperHeroe as Heroe } from './extra/classes';
+
+/*
+    Objetivo:
+        Cambiar únicamente la refencia de SuperHeroe a Heroe
+        OJO!: Pero no reemplazar los strings
+
+    Tips:
+        Replace Symbol
+        F2
+*/
+
+
+const wolverine = new Heroe();
+const ironman   = new Heroe();
+const spiderman = new Heroe();
+
+function saludar() {
+    return 'El SuperHeroe Wolverine es genial!';
+}
+
+function gritar() {
+    return 'El SuperHeroe en este string no se debe de cambiar';
+}
+
---
+++ Curso Visual Studio Code/Edicion basica/08-deshacer-rehacer.ts
@@ -0,0 +1,26 @@
+/*
+    Objetivo:
+        Deshacer y rehacer el código
+§
+    Tips:
+        ⌘ Z
+        ⌘ ⇧ Z
+        Ctrl +Z
+        Ctrl + Shift + Z
+
+*/
+
+function holaMundo(){
+    return 'saludos a todos!'
+}
+
+
+
+
+// Demo
+// function holaMundo() {
+//     return 'Saludos a todos!';
+// }
+
+
+// Done!!
